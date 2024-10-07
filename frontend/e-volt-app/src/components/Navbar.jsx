c046170f91272b60d549b9a0fa1528e63072c15c
Abolare Roheemah <roheemahabo@gmail.com>
2024-10-07 22:33:19 +0100

    few changes

--- frontend/e-volt-app/src/components/Navbar.jsx
+++ frontend/e-volt-app/src/components/Navbar.jsx
@@ -91,16 +91,19 @@ export default function Onboard() {
             <div className="flex ml-20">
                 <ul className="flex gap-16">
                 <li className={`px-2 text-[#8F96A1] font-medium hover:border-b-2 hover:border-[#5773fb] hover:text-[#5773fb] nav-links ${router.pathname === '/' ? 'active' : ''}`} id="dashboard">
-                    <Link className="" href="/">Dashboard</Link>
+                    <Link className="" href="/">Home</Link>
                 </li>
                 <li className={`px-2 text-[#8F96A1] font-medium hover:border-b-2 hover:border-[#5773fb] hover:text-[#5773fb] nav-links ${router.pathname === '/elections' ? 'active' : ''}`} id="elections">
-                    <Link className="" href="/elections">Vote</Link>
+                    <Link className="" href="/elections">All Elections</Link>
                 </li>
                 {/* <li className="px-8  nav-linksfont-medium">
                     <Link hre className=""f="/kyc">KYC</Link>
                 </li> */}
                 <li className={`px-2 text-[#8F96A1] font-medium hover:border-b-2 hover:border-[#5773fb] hover:text-[#5773fb] nav-links ${router.pathname === '/addElection' ? 'active' : ''}`} id="addElection">
                     <Link className="" href="/addElection">Create Election</Link>
+                </li>
+                <li className={`px-2 text-[#8F96A1] font-medium hover:border-b-2 hover:border-[#5773fb] hover:text-[#5773fb] nav-links ${router.pathname === '/addElection' ? 'active' : ''}`} id="addElection">
+                    <Link className="" href="/addElection">Voting History</Link>
                 </li>
                 </ul>
             </div>
